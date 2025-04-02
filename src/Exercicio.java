d2f35a773e090f64838980fb7c88c5621e9f8b40
Victhor <victhorgabriel00@live.com>
2025-04-02 17:20:38 -0300

    first commit

---
+++ src/Exercicio.java
@@ -0,0 +1,16 @@
+public class Exercicio {
+
+    public static void main(String args[]) {
+        numeros ();
+    }
+
+   public static void numeros (){
+
+       int codigo = 10;
+       System.out.println(codigo );
+
+       Integer codigo1 = 20;
+       System.out.println(codigo1);
+   }
+
+}
